5fb27b4350770ed93fcec23da880cbd1df1b38e2
chenjishuang <jishuang@usc.edu>
2023-11-10 15:57:07 -0800

    Update Gz.h

--- Gz.h
+++ Gz.h
@@ -46,11 +46,11 @@ typedef struct Vertex
     double color_specular[3];
     double normal[3];
     double shininess;
-}Vertex;
+}GzVertex;
 
 typedef struct Triangle
 {
-    Vertex v[3];
+    GzVertex v[3];
 }GzTriangle;
 
 typedef struct Sphere
